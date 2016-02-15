c5564465e69a1be4b87bbfd326b26fdb87543db2
Wally <wally@imwally.net>
2016-02-15 00:58:46 -0500

    Add Logger(), update Albums(), clean up names.
    * Logger() prints request information to stdout.
    * Albums() now filters response but artist by taking the last string in
      the path (i.e. /albums/artist_name).
    * More descriptive variable names.

--- apiserver.go
+++ apiserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -16,30 +17,55 @@ type API struct {
 // endPoints describes api end points.
 var endPoints = map[string]string{
 	"all_artists":   "/artists",
-	"albums":        "/albums",
+	"albums":        "/albums{/artist_name}",
 	"album_artwork": "/artwork/{track_path}",
+}
+
+// Logger is a helper function that prints HTTP request information to
+// stdout.
+func Logger(r *http.Request) {
+	log.Println(r.RemoteAddr, r.Method, r.Proto, r.RequestURI)
 }
 
 // Index responds with a map of api end points.
 func (api *API) Index(w http.ResponseWriter, r *http.Request) {
+	Logger(r)
 	e := json.NewEncoder(w)
 	if err := e.Encode(endPoints); err != nil {
 		http.Error(w, fmt.Sprintf("%s\n", err), 400)
 	}
 }
 
-// Artists responds with json encoded Library.Artists struct.
+// Artists responds with a json encoded Library.Artists struct
+// containing all artists.
 func (api *API) Artists(w http.ResponseWriter, r *http.Request) {
+	Logger(r)
 	e := json.NewEncoder(w)
 	if err := e.Encode(api.Library.Artists); err != nil {
 		http.Error(w, fmt.Sprintf("%s\n", err), 400)
 	}
 }
 
-// Albums reponds with a json encoded Library.Albums struct.
+// Albums reponds with a json encoded Library.Albums struct containing
+// all albums. If an artist name procedes the /albums/ path then only
+// albums from that artist will be returned.
 func (api *API) Albums(w http.ResponseWriter, r *http.Request) {
+	Logger(r)
+	artistName := strings.Replace(r.URL.String(), "/albums/", "", -1)
+	unescapedArtistName, err := url.QueryUnescape(artistName)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s\n", err), 400)
+	}
+
+	var response interface{}
+	if unescapedArtistName != "" {
+		response = api.Library.AlbumsBy(unescapedArtistName)
+	} else {
+		response = api.Library.Albums
+	}
+
 	e := json.NewEncoder(w)
-	if err := e.Encode(api.Library.Albums); err != nil {
+	if err := e.Encode(response); err != nil {
 		http.Error(w, fmt.Sprintf("%s\n", err), 400)
 	}
 }
@@ -47,15 +73,16 @@ func (api *API) Albums(w http.ResponseWriter, r *http.Request) {
 // Artwork takes the path after /artwork/ and responds with the album
 // cover of the track found at that path.
 func (api *API) Artwork(w http.ResponseWriter, r *http.Request) {
-	location := strings.Trim(r.URL.String(), "/artwork/")
-	unescapedLocation, err := url.QueryUnescape(location)
+	Logger(r)
+	trackPath := strings.Trim(r.URL.String(), "/artwork/")
+	unescapedTrackPath, err := url.QueryUnescape(trackPath)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("%s\n", err), 400)
 	}
 
-	art, err := AlbumArt("/" + unescapedLocation)
+	art, err := AlbumArt("/" + unescapedTrackPath)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("no artwork for: %s\n", unescapedLocation), 400)
+		http.Error(w, fmt.Sprintf("no artwork for: %s\n", unescapedTrackPath), 400)
 	}
 
 	w.Write(art)
@@ -67,7 +94,11 @@ func ServeAPI(lib *Library) {
 
 	http.HandleFunc("/", api.Index)
 	http.HandleFunc("/artists", api.Artists)
+	http.HandleFunc("/artists/", api.Artists)
 	http.HandleFunc("/albums", api.Albums)
+	http.HandleFunc("/albums/", api.Albums)
 	http.HandleFunc("/artwork/", api.Artwork)
-	http.ListenAndServe(":8080", nil)
+
+	log.Println("Blaster API server started on port :8080.")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
